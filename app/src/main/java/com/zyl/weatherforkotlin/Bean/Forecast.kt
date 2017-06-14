7c13cc201edc17f40e55afde3e03f2de5ee53666
zouyulong <zouyulong@syswin.com>
2017-06-14 18:43:29 +0800

    添加图标

---
+++ app/src/main/java/com/zyl/weatherforkotlin/Bean/Forecast.kt
@@ -0,0 +1,14 @@
+package com.zyl.weatherforkotlin.Bean
+
+import java.util.*
+
+/**
+ * Description: 天气预报bean类
+ * Created by zouyulong on 2017/6/14.
+ * Job number:147490
+ * Phone : 15810880928
+ * Email : zouyulong@syswin.com
+ * Person in charge :  zouyulong
+ * Leader : lixiao
+ */
+data class Forecast(val date: Date, val temperature: Float, val details:String)
